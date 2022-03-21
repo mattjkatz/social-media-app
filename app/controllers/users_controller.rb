8696b04c1a8ece435a463be4f4a4c001d6875d30
mattjkatz <matthewkatz7@gmail.com>
2022-03-21 15:58:18 -0500

    add methods for create and show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,25 @@ class UsersController < ApplicationController
     render json: all_users
   end
 
+  def create
+    user = User.new(
+      name: params["name"],
+      username: params["username"],
+      bio: params["bio"],
+      email: params["email"]
+    )
+    if user.save
+      render json: user
+    else
+      render json: {error_message: user.errors.full_messages}, status: 422
+    end
+  end
+
+  def show
+    user_id = params["id"]
+    all_users = User.all
+    searched_user = all_users.find(user_id)
+    render json: searched_user
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  validates :name, presence: true
+  validates :username, presence: true, uniqueness: true
+  validates :bio, length: { maximum: 200 }
+  validates :email, presence: true, uniqueness: true
 end
